c381c2126d59056f61cb196241d4481bb846b91e
Evgheni Nichitenco <nichitenco@outlook.com>
2018-05-10 22:32:19 +0300

    add plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const browsersync =require('browser-sync').create();
 const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const spritesmith = require('gulp.spritesmith');
+const rimraf = require('rimraf');
 
 // Server
 
@@ -27,3 +30,29 @@ gulp.task('templates:compile', function buildHTML() {
         .pipe(gulp.dest('build'))
 });
 
+// Styles compile
+
+gulp.task('sass', function () {
+    return gulp.src('source/styles/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('build/css'));
+});
+
+// Sprite
+
+gulp.task('sprite', function (cb) {
+    const spriteData = gulp.src('source/images/*.png').pipe(spritesmith({
+        imgName: 'sprite.png',
+        imgPath: '../images/sprite.png',
+        cssName: 'sprite.scss'
+    }));
+    spriteData.img.pipe(gulp.dest('build/images/'));
+    spriteData.css.pipe(gulp.dest('source/styles/global/'));
+    cb();
+});
+
+// Delete
+
+gulp.task('clean', function del(cb) {
+   return rimraf('build', cb);
+});
